c170bfa68fd4cfed4c825d4727c4614563a14e7e
Brad Oyler <bradoyler@gmail.com>
2017-04-01 12:00:53 -0400

    add assertion test - expected svg file - output callback

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const svg2png = require('svg2png');
 
-module.exports = function (dest, d3n) {
+module.exports = function (dest, d3n, callback) {
   const d3 = d3n.d3;
 
   function eachGeoQuantize (d) {
@@ -12,15 +12,20 @@ module.exports = function (dest, d3n) {
 
   // reduce size of svg
   d3n.d3Element.selectAll('path').each(eachGeoQuantize);
-  const svgString = d3n.svgString();
 
   fs.writeFile(`${dest}.html`, d3n.html(), function () {
     console.log(`>> Exported "${dest}.html", open in a web browser`)
   });
 
+  const svgString = d3n.svgString();
+
   fs.writeFile(`${dest}.svg`, svgString, function () {
     console.log(`>> Exported "${dest}.svg"`);
   });
+
+  if (typeof callback === 'function') { // used for testing
+    callback(d3n);
+  }
 
   var svgBuffer = new Buffer(svgString, 'utf-8');
   svg2png(svgBuffer)
--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 const pieChart = require('d3node-piechart');
 const fs = require('fs');
+const assert = require('assert');
 const d3 = require('d3-node')().d3;
 const output = require('../index');
 const csvString = fs.readFileSync('test/data.csv').toString();
@@ -10,4 +11,13 @@ const chart = pieChart(data);
 // test camelCase in svgString
 chart.d3Element.select('svg').append('radialGradient').attr('offset', '0%');
 
-output('dist/test', chart);
+output('dist/test', chart, (d3n) => {
+  // assertion test
+  try {
+    const expectedSvg = fs.readFileSync('test/expected.svg', 'utf-8');
+    assert.equal(d3n.svgString(), expectedSvg);
+  } catch (ex) {
+    console.log(ex);
+  }
+});
+
